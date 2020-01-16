d05fa36077b05bfcc9362b2016de8838de301f80
Bryan Fauquembergue <bryan.fauquembergue@etu.univ-smb.fr>
2020-01-16 14:19:50 +0100

    Missing Encoding

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ import requests
 requests.get(url+'/ftp/acquisitions.md')
 # ---- Error Handling (Provoke an error that is neither very gracefully nor consistently handled.)
 requests.get(url+'/rest/qwertz')
+# ---- Missing Encoding (Retrieve the photo of Bjoern's cat in "melee combat-mode".)
+requests.get(url+'/assets/public/images/uploads/😼-%23zatschi-%23whoneedsfourlegs-1572600969477.jpg')
